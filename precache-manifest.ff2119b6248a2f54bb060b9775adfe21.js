4e3862873fa08d42489bcfd89513729e073f0717
Deepak Bisht <deepakb2@dewsolutions.in>
2021-07-23 21:18:54 +0530

    Updates

---
+++ precache-manifest.ff2119b6248a2f54bb060b9775adfe21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25c209055c36e3b3545a415fc808cc8",
+    "url": "/fashion-portfolio/index.html"
+  },
+  {
+    "revision": "b3fb73a3f99f75ba1d7a",
+    "url": "/fashion-portfolio/static/css/2.c301f1a7.chunk.css"
+  },
+  {
+    "revision": "b3134eda36e1fd69396c",
+    "url": "/fashion-portfolio/static/css/main.a3f1dba6.chunk.css"
+  },
+  {
+    "revision": "b3fb73a3f99f75ba1d7a",
+    "url": "/fashion-portfolio/static/js/2.d7e2dacd.chunk.js"
+  },
+  {
+    "revision": "7bcd44f458a25f30193138277981d459",
+    "url": "/fashion-portfolio/static/js/2.d7e2dacd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3134eda36e1fd69396c",
+    "url": "/fashion-portfolio/static/js/main.9c91891b.chunk.js"
+  },
+  {
+    "revision": "b5700d1824abc40ee461",
+    "url": "/fashion-portfolio/static/js/runtime-main.eaac91d9.js"
+  }
+]);
